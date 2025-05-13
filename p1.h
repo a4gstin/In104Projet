a2ba5ddddfdadb8663db36637a5b35e899373093
MASSOT Romain <romain.massot@info1-new.ensta.fr>
2025-05-13 15:44:04 +0200

    adding p1.h for the separated compilation p1.c and main3.c

---
+++ p1.h
@@ -0,0 +1,22 @@
+#ifndef __P1_H__
+#define __P1_H__
+
+#include <stdlib.h>
+
+// Declaration of the functions we are coding in p1.c
+
+int findUserIndex(const char* name) ;
+
+void printPath(int* path, int length) ;
+
+void initializeGraph() ;
+
+void addUserWithFriendships(const char* name, int friendCount, const char** friendNames, const int* strengths) ;
+
+void removeUser(const char* name) ;
+
+void modifyFriendship(const char* name1, const char* name2, int strength) ;
+
+int findShortestPath(int start, int end, int* path) ;
+
+#endif
